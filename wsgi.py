5df3b3b2929d72870131ad6d1a655dbe5a97b776
wisdom-kwaku <amoatengwisdom18@gmail.com>
2025-02-19 15:22:36 +0000

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,8 @@
+import sys
+
+# Add the path to your project to PYTHONPATH
+sys.path.append('/home/Kwaku/mysite')
+
+from flask_app import app  # Replace flask_app with your Flask file name
+
+application = app
